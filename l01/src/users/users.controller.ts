313740aab601a8b9033805673f46f60f80a33d24
GulsumStc <121710851+GulsumStc@users.noreply.github.com>
2024-06-18 17:14:42 +0300

    Create users module using 'nest g module users / nest g controller users / nest g service users commands'

---
+++ l01/src/users/users.controller.ts
@@ -0,0 +1,13 @@
+import { Controller } from '@nestjs/common';
+
+@Controller('users') // decorator 
+export class UsersController {
+
+  /* 
+  GET users
+  GET users/:id
+  POST users
+  PATCH users/:id
+  DELETE users/:id
+   */
+}
