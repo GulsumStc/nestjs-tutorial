b32110015a79405eb08280a607580f7e5dd698b1
GulsumStc <121710851+GulsumStc@users.noreply.github.com>
2024-06-19 12:46:57 +0300

    Create methods in user service Create, Read, Update, Delete, and beacuse of no connection with DB, created users objects

--- l01/src/users/users.service.ts
+++ l01/src/users/users.service.ts
@@ -1,4 +1,93 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+
+  private users =  [
+    {
+      id: 1,
+      name: 'John Doe',
+      email: 'john.doe@example.com',
+      role: 'INTERN'
+    },
+    {
+      id: 2,
+      name: 'Jane Smith',
+      email: 'jane.smith@example.com',
+      role: 'INTERN'
+    },
+    {
+      id: 3,
+      name: 'Mike Johnson',
+      email: 'mike.johnson@example.com',
+      role: 'INTERN'
+    },
+    {
+      id: 4,
+      name: 'Emily Davis',
+      email: 'emily.davis@example.com',
+      role: 'ENGINEER'
+    },
+    {
+      id: 5,
+      name: 'Chris Lee',
+      email: 'chris.lee@example.com',
+      role: 'ADMIN'
+    }
+  ];
+
+
+  findAll(role?: 'INTERN' | 'ENGINEER' | 'ADMIN') {
+    if (role) {
+      return this.users.filter(user => user.role === role);
+    }
+    return this.users;
+    
+  }
+
+  findOne(id: number) {
+    const user = this.users.find(user => user.id === id);
+    return user;
+  }
+
+  create(user: {name: string, email: string, role: 'INTERN' | 'ENGINEER' | 'ADMIN'}) {
+   
+    const usersByHighestID = [...this.users].sort((a, b) => b.id - a.id); //
+    console.log(usersByHighestID);
+    const newUsers = {...user, id: usersByHighestID[0].id + 1};
+    this.users.push(newUsers);
+    return this.users;
+
+  }
+
+  /**
+   * Update a user with the given id with the provided update user data.
+   * If the update user data contains a name, email, or role, it will be updated.
+   * If the update user data does not contain any of these fields, the user will not be updated.
+   * 
+   * @param id The id of the user to update.
+   * @param updateUser An object containing optional name, email, and role fields to update the user with.
+   * @returns The updated user object with the matching id.
+   */
+  update(id: number, updateUser: { name?: string, email?: string, role?: 'INTERN' | 'ENGINEER' | 'ADMIN' }) {
+    // Loop through each user and update the user with the matching id
+    this.users = this.users.map(user => {
+      if (user.id === id) {
+        // If the user has a matching id, update the user with the provided update user data
+        return {...user, ...updateUser};
+      }
+      return user;
+    })
+
+    // Return the updated user object with the matching id
+    return this.findOne(id);
+  }
+
+  delete(id: number) {
+    const removeUser = this.findOne(id);
+    this.users = this.users.filter(user => user.id !== id);
+    return removeUser;
+
+  }
+  
+}
