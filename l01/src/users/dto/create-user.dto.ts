8b5ac29416e9b7ec14bca5f9304f0843e45d6ec0
GulsumStc <121710851+GulsumStc@users.noreply.github.com>
2024-06-20 17:50:13 +0300

    class-validator / class-transformer ValidationPipe installed  class-validator class-transformer. added some decorators (IsEmail, IsEnum, IsNotEmpty, IsString) in CreateUserDto and used ValidationPipe in @Body decorator at UserController  to automate the validation and transformation processes for incoming requests

--- l01/src/users/dto/create-user.dto.ts
+++ l01/src/users/dto/create-user.dto.ts
@@ -1,5 +1,13 @@
+import { IsEmail, IsEnum, IsNotEmpty, IsString } from "class-validator";
+
 export class CreateUserDto {
+  @IsString()
+  @IsNotEmpty({ message: 'name is required' })
   name: string;
+  @IsEmail()
   email: string;
+  @IsEnum(['INTERN', 'ENGINEER', 'ADMIN'], {
+    message: 'role must be one of INTERN, ENGINEER, ADMIN',
+  })
   role: 'INTERN' | 'ENGINEER' | 'ADMIN';
 }
