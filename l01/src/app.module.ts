f01fbe9bf786651778fffb1586f4e9ef0c8a4257
GulsumStc <121710851+GulsumStc@users.noreply.github.com>
2024-06-21 21:39:51 +0300

    install prisma, connect with database, create module Prisma and Prisma Client installed, database connected, employee module created.

--- l01/src/app.module.ts
+++ l01/src/app.module.ts
@@ -2,9 +2,12 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
+import { DatabaseModule } from './database/database.module';
+import { EmployeesModule } from './employees/employees.module';
+import { AnimalsModule } from './animals/animals.module';
 
 @Module({
-  imports: [UsersModule],
+  imports: [UsersModule, DatabaseModule, EmployeesModule, AnimalsModule],
   controllers: [AppController],
   providers: [AppService],
 })
