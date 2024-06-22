ba22a2fee7782ed0a319ce05b2c65c39e37bac9c
GulsumStc <121710851+GulsumStc@users.noreply.github.com>
2024-06-22 19:57:13 +0300

    install TypeORM pg dotenv bcrypt we created connection with PostgreSQL, database which is testDB, created user entity(table) installed bcrypt to hash password

---
+++ l02/ormconfig.ts
@@ -0,0 +1,21 @@
+import { PostgresConnectionOptions } from "typeorm/driver/postgres/PostgresConnectionOptions";
+import { config as dotenvConfig } from 'dotenv';
+// import { User } from "src/entities/user.entity";
+import { join } from 'path';
+
+dotenvConfig(); // .env dosyasını yükler
+
+const config: PostgresConnectionOptions = {
+  type: "postgres",
+  database: "testDB",
+  host: "localhost",
+  port: 5432,
+  username: process.env.DATABASE_USERNAME,
+  password: process.env.DATABASE_PASSWORD,
+  // entities: [User],
+  entities: [join(__dirname, '**', '*.entity.{ts,js}')], 
+  synchronize: true,
+  
+}
+
+export default config;
