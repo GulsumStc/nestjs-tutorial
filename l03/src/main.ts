5e00bcc993a05e1ecaac7eb66c3e3a3f2671f1a8
GulsumStc <121710851+GulsumStc@users.noreply.github.com>
2024-06-26 12:20:08 +0300

    generated Users module and controller

--- l03/src/main.ts
+++ l03/src/main.ts
@@ -1,10 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+/* 
 
+main.ts file is entrypoint of the application. All initialized modules will be loaded here.
+register middleware, configure librraries like  passport are done here
+
+*/
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  // Start listening for incoming requests on port 3000.
+  await app.listen(3001);
 }
 bootstrap();
